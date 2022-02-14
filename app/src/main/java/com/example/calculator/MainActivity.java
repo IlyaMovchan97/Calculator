35ece118173a5e48ebb5f434e95dc626fc61e1bb
Movchan <Movchanilya0216@mail.ru>
2022-02-14 17:31:15 +0300

    - Redesigned the buttons into a material design - All sizes are made by resources - Created the style of application

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -19,9 +20,12 @@ public class MainActivity extends AppCompatActivity {
     private char CURRENT_ACTION;
 
 
+
     private TextView window, signbox;
+    private TextView edit_text;
 
     private Button but_zero;
+    private Button but_double_zero;
     private Button but_1;
     private Button but_2;
     private Button but_3;
@@ -31,10 +35,12 @@ public class MainActivity extends AppCompatActivity {
     private Button but_7;
     private Button but_8;
     private Button but_9;
+    private Button but_point;
 
     private Button but_clean;
     private Button but_part;
     private Button but_mult;
+    private Button but_back;
     private Button but_share;
     private Button but_diff;
     private Button but_sum;
@@ -48,10 +54,12 @@ public class MainActivity extends AppCompatActivity {
 
         window = findViewById(R.id.window);
         signbox = findViewById(R.id.window);
+        edit_text = findViewById(R.id.edit_text);
 
 
 
         but_zero = findViewById(R.id.but_zero);
+        but_double_zero = findViewById(R.id.but_double_zero);
         but_1 = findViewById(R.id.but_1);
         but_2 = findViewById(R.id.but_2);
         but_3 = findViewById(R.id.but_3);
@@ -61,10 +69,12 @@ public class MainActivity extends AppCompatActivity {
         but_7 = findViewById(R.id.but_7);
         but_8 = findViewById(R.id.but_8);
         but_9 = findViewById(R.id.but_9);
+        but_point = findViewById(R.id.but_point);
 
         but_clean = findViewById(R.id.but_clean);
         but_part = findViewById(R.id.but_part);
         but_mult = findViewById(R.id.but_mult);
+        but_back = findViewById(R.id.but_back);
         but_share = findViewById(R.id.but_share);
         but_diff = findViewById(R.id.but_diff);
         but_sum = findViewById(R.id.but_sum);
@@ -75,6 +85,13 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 window.setText(window.getText() + "0");
+            }
+        });
+
+        but_double_zero.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                window.setText(window.getText() + "00");
             }
         });
 
@@ -143,11 +160,36 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        /**
+         * Обработка кнопок операций:
+         */
+
+        but_point.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                /*
+                String text = (String) window.getText();
+
+                window.getText();
+
+                void IsNotEmpty (String text){
+                  return;
+                };
+
+                if (window.getText().) {
+
+                 */
+                    window.setText(window.getText() + ".");
+                }
+        });
+
 
         but_clean.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 window.setText("");
+                edit_text.setText("");
             }
         });
 
@@ -161,7 +203,37 @@ public class MainActivity extends AppCompatActivity {
         but_mult.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                window.setText(window.getText() + "*");
+                String a;
+                window.setText(a = window.getText() + "*");
+                String b;
+
+                /*
+                int d = Integer.parseInt(a);
+                int e = Integer.parseInt(b);
+                int c = d * e;
+                edit_text.setText(c);
+
+                 */
+
+            }
+        });
+
+        but_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                window.setText("");
+                edit_text.setText("");
+
+                /*
+                String a;
+                String b;
+                window.setText(a= (String) window.getText());
+                b=a.substring(-1);
+                window.setText(window.getText(a.substring(0, b.length(1,))));
+
+                 */
+
             }
         });
 
